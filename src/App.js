2762558479e58b867cc1f1bb87646f13fd384990
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-18 11:43:08 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import './App.css';
+import styles from './styles/home.module.css'
 import { Link } from "react-router-dom"
 import { Outlet } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
+    <div className={styles.app}>
+      <nav className={styles.nav}>
         <Link to="/Home">Home</Link> |{" "}
         <Link to="/Projects">Projects</Link> |{" "}
         <Link to="/Contact">Contact</Link>
       </nav>
       <Outlet/>
-      <h1>Hello Jessy</h1>
+      <h1 className={styles.header}>Hello Jessy</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <Routes>
+  <Route path="/" element={<App />} />
   <Route path="Home" element={<App />} />
   <Route path="contact" element={<Contact />} />
   <Route path="projects" element={<Projects />} />
