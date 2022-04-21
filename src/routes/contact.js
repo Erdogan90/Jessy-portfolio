da1759a9a3f030cf1c9d4ad59f353f500993dd0f
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-21 12:45:00 +0100

    emailjs

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import ContactForm from "../components/Contact";
 import css from "../styles/contact.module.css"
+import emailjs from 'emailjs-com';
 
 function Contact() {
     return (
