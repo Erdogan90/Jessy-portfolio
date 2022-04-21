b9f702cc5dc56666a48603a37c30f96aab5224ba
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-21 11:49:15 +0100

    additional pages

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,17 +1,13 @@
 import { Link } from "react-router-dom"
+import css from "../styles/contact.module.css"
 
 function Contact() {
     return (
-      <div style={{ padding: "1rem 0" }}>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/Home">Home</Link> |{" "}
-        <Link to="/Projects">Projects</Link> |{" "}
-        <Link to="/Contact">Contact</Link>
+<div className={css.page}>
+            <nav className={css.nav}>
+        <Link className={css.link} to="/Home">Home</Link> |{" "}
+        <Link className={css.link} to="/Projects">Projects</Link> |{" "}
+        <Link className={css.link} to="/Contact">Contact</Link>
       </nav>
         <h2>Contact</h2>
       </div>
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,17 +1,13 @@
 import { Link } from "react-router-dom"
+import css from "../styles/projects.module.css"
 
 function Projects() {
     return (
-      <div style={{ padding: "1rem 0" }}>
-            <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/Home">Home</Link> |{" "}
-        <Link to="/Projects">Projects</Link> |{" "}
-        <Link to="/Contact">Contact</Link>
+      <div className={css.page}>
+            <nav className={css.nav}>
+        <Link className={css.link} to="/Home">Home</Link> |{" "}
+        <Link className={css.link} to="/Projects">Projects</Link> |{" "}
+        <Link className={css.link} to="/Contact">Contact</Link>
       </nav>
         <h2>Projects</h2>
       </div>
