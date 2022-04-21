f51b06fb1723d5eff045c9599deb456388374ec8
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-21 11:15:17 +0100

    clean up css on homepage

--- src/App.js
+++ src/App.js
@@ -2,33 +2,28 @@ import styles from './styles/home.module.css'
 import { Link } from "react-router-dom"
 import { Row, Col} from 'react-bootstrap';
 import Brands from './components/Brands/index';
-import Hero from './components/Hero';
+
 
 
 
 function App() {
   return (
     <div className={styles.page}>
-    <Row>
-    <Col className={styles.nav}>
-      <nav>
+    <Row >
+      <nav className={styles.nav}>
         <Link className={styles.link}to="/Home">Home</Link> |{" "}
         <Link className={styles.link} to="/Projects">Projects</Link> |{" "}
         <Link className={styles.link} to="/Contact">Contact</Link>
       </nav>
-      </Col>
       </Row>
       <Row className={styles.main}>
       <Col>
-      <Hero/>
-      {/* <h3>"What, Who, Where & How.</h3>
-      <h4>Helping businesses all over the world share their stories & grow their potential. One word at a time."</h4> */}
+      <h3>"What, Who, Where & How.</h3>
+      <h4>Helping businesses all over the world share their stories & grow their potential. One word at a time."</h4>
       </Col>
       </Row>
-      <Row className={styles.brands}>
-      <Col>
+      <Row className={styles.brands} >
       <Brands/>
-      </Col>
       </Row>
     </div>
   );
