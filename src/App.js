6c2405c90289fbbc50125d8314d5e1fe5f515342
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-22 13:29:17 +0100

    borders

--- src/App.js
+++ src/App.js
@@ -9,14 +9,12 @@ import Brands from './components/Brands/index';
 function App() {
   return (
     <div className={styles.page}>
-    <Row >
+    <Row className={styles.text}>
       <nav className={styles.nav}>
         <Link className={styles.link}to="/Home">Home</Link> |{" "}
         <Link className={styles.link} to="/Projects">Projects</Link> |{" "}
         <Link className={styles.link} to="/Contact">Contact</Link>
       </nav>
-      </Row>
-      <Row className={styles.text}>
       <h3>"What, Who, Where & How.</h3>
       <h4>Helping businesses all over the world share their stories & grow their potential. One word at a time."</h4>
       <div className={styles.curve}></div>
