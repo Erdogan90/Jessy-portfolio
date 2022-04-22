1f451c72f0dfe085ad136797f2113c8449ae1ac5
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-22 12:55:23 +0100

    wave one

--- src/App.js
+++ src/App.js
@@ -19,8 +19,13 @@ function App() {
       <Row className={styles.text}>
       <h3>"What, Who, Where & How.</h3>
       <h4>Helping businesses all over the world share their stories & grow their potential. One word at a time."</h4>
+      <div className={styles.curve}></div>
       </Row>
-      <Row className={styles.Brands} >
+      <Row className={styles.about}>
+      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      <div className={styles.curvetwo}></div>
+      </Row>
+      <Row className={styles.brands} >
       <Brands/>
       </Row>
     </div>
