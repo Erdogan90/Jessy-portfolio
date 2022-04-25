9987a1d5dcbcb31dc2d38ff4383bc742aa90ac9f
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-25 11:42:00 +0100

    initial-card

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import css from "../styles/projects.module.css"
+import { Card, Button } from "react-bootstrap";
 
 function Projects() {
     return (
@@ -10,6 +11,19 @@ function Projects() {
         <Link className={css.link} to="/Contact">Contact</Link>
       </nav>
         <h2>Projects</h2>
+
+        <Card style={{ width: '20rem' }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1500989145603-8e7ef71d639e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80" />
+  <Card.Body>
+    <Card.Title>Blog Work</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
       </div>
     );
   }
