24ba84af2bacca8d23e3d1064c498bd12c5b60fb
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-26 12:06:24 +0100

    component and props

--- src/components/Project-Card/index.js
+++ src/components/Project-Card/index.js
@@ -2,12 +2,12 @@ import { Card, Button } from "react-bootstrap";
 import React from "react";
 
 
-function ProjectCard(){
+function ProjectCard({image, title}){
     return <div>
     <Card style={{ width: '20rem' }}>
-    <Card.Img variant="top" src="https://images.unsplash.com/photo-1500989145603-8e7ef71d639e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80" />
+    <Card.Img variant="top" src={image} />
     <Card.Body>
-    <Card.Title>Blog Work</Card.Title>
+    <Card.Title>{title}</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom"
 import css from "../styles/projects.module.css"
 import { Card, Button } from "react-bootstrap";
+import ProjectCard from "../components/Project-Card";
+
 
 function Projects() {
     return (
@@ -11,19 +13,8 @@ function Projects() {
         <Link className={css.link} to="/Contact">Contact</Link>
       </nav>
         <h2>Projects</h2>
-
-        <Card style={{ width: '20rem' }}>
-  <Card.Img variant="top" src="https://images.unsplash.com/photo-1500989145603-8e7ef71d639e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80" />
-  <Card.Body>
-    <Card.Title>Blog Work</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-
+        <ProjectCard title={'Blog work'} image={"https://images.unsplash.com/photo-1500989145603-8e7ef71d639e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80"}/>
+        <ProjectCard title={'Digital Marketing'} image={"https://images.unsplash.com/photo-1432888622747-4eb9a8efeb07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"}/>
       </div>
     );
   }
