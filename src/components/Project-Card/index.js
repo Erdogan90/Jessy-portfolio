fd89e711a6544c9b4d447e72a799bb01c599c2e4
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-26 16:07:30 +0100

    adding the buttons

--- src/components/Project-Card/index.js
+++ src/components/Project-Card/index.js
@@ -1,5 +1,6 @@
 import { Card, Button } from "react-bootstrap";
 import React from "react";
+import css from "../../styles/projectcard.module.css"
 
 
 function ProjectCard({image, title}){
@@ -12,7 +13,8 @@ function ProjectCard({image, title}){
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Button className={css.button} variant="primary">Go somewhere</Button>
+    <Button className={css.button} variant="primary">Go somewhere</Button>
     </Card.Body>
     </Card>
 </div>
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -12,13 +12,13 @@ function Projects() {
         <Link className={css.link} to="/Projects">Projects</Link> |{" "}
         <Link className={css.link} to="/Contact">Contact</Link>
       </nav>
-        <h2>Projects</h2>
+        <h2 className={css.header}>Projects</h2>
         <Row className={css.info}>
         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </Row>
-        <Row noGutters={true}>
+        <Row className={css.row}>
         <Col className={css.col}>
-        <ProjectCard title={'Blog work'} image={"https://images.unsplash.com/photo-1500989145603-8e7ef71d639e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80"}/>
+        <ProjectCard title={'Blog Work'} image={"https://images.unsplash.com/photo-1500989145603-8e7ef71d639e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80"}/>
         </Col>
         <Col className={css.col}>
         <ProjectCard title={'Digital Marketing'} image={"https://images.unsplash.com/photo-1432888622747-4eb9a8efeb07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80"}/>
@@ -27,7 +27,13 @@ function Projects() {
         <ProjectCard title={'E-Commerce'} image={"https://images.unsplash.com/photo-1556740714-a8395b3bf30f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}/>
         </Col>
         <Col className={css.col}>
-        <ProjectCard title={'Ideation'} image={"https://images.unsplash.com/photo-1609372697655-65d874e3fe60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}/>
+        <ProjectCard title={'Ideation & Brand Development'} image={"https://images.unsplash.com/photo-1609372697655-65d874e3fe60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}/>
+        </Col>
+        <Col className={css.col}>
+        <ProjectCard title={'Web Copy'} image={"https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}/>
+        </Col>
+        <Col className={css.col}>
+        <ProjectCard title={'Print'} image={"https://images.unsplash.com/photo-1644952350841-070996fad2af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}/>
         </Col>
         </Row>
       </div>
