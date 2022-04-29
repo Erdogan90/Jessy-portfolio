36c5f878ebc0f40ff5fdaec4ac3f6d7419b1fea9
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-29 12:54:38 +0100

    adding link

--- src/routes/rokapdf.js
+++ src/routes/rokapdf.js
@@ -1,4 +1,4 @@
-// import pdf from "../Pdf/sample_work_website.pdf"
+import pdf from "../Pdf/sample_work_website.pdf"
 import React from "react";
 import css from "../styles/rokapdf.module.css"
 
@@ -6,10 +6,10 @@ import css from "../styles/rokapdf.module.css"
 function RokaPdf() {
     return (
 <div className={css.div}>
-{/* <embed className={css.pdf}
+<embed className={css.pdf}
     src={pdf + "#toolbar=0"}
     type="application/pdf"
-  /> */}
+  />
 
       </div>
     );
