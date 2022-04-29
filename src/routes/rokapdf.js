db4f7fc2a4fb3bfc35e491a3be8592522a3b6d26
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-29 14:53:14 +0100

    pdf complete

--- src/routes/rokapdf.js
+++ src/routes/rokapdf.js
@@ -1,16 +1,51 @@
 import pdf from "../Pdf/sample_work_website.pdf"
 import React from "react";
 import css from "../styles/rokapdf.module.css"
+import {Document, Page, pdfjs} from "react-pdf"
+import { useState } from "react";
+import {Button} from "react-bootstrap"
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function RokaPdf() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  const nextPage = () => {
+    if (pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
     return (
 <div className={css.div}>
-<embed className={css.pdf}
-    src={pdf + "#toolbar=0"}
-    type="application/pdf"
-  />
 
+  <Document className={css.pdf} file={pdf} 
+    onLoadSuccess={onDocumentLoadSuccess}
+    onContextMenu={(e) => e.preventDefault()}>
+  <Page pageNumber={pageNumber} />
+  </Document>
+  <div className={css.controls}>
+  <Button variant="dark" className={css.buttons} onClick={prevPage} disabled={pageNumber === 1}>
+          Prev
+  </Button>
+        <Button variant="dark" className={css.buttons} onClick={nextPage} disabled={pageNumber === numPages}>
+          Next
+        </Button>
+  </div>
+      <p className={css.page}>
+        Page {pageNumber} of {numPages}
+      </p>
       </div>
     );
   }
