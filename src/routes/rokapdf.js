2722de77f1ead8592a9039d4b6d2cd189849d0d3
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-29 12:17:43 +0100

    test of button

--- src/routes/rokapdf.js
+++ src/routes/rokapdf.js
@@ -6,10 +6,10 @@ import css from "../styles/rokapdf.module.css"
 function RokaPdf() {
     return (
 <div className={css.div}>
-<embed className={css.pdf}
+{/* <embed className={css.pdf}
     src={pdf + "#toolbar=0"}
     type="application/pdf"
-  />
+  /> */}
 
       </div>
     );
