93dc362102280306e1f3b7e14f6d7e6be1a78cf7
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-04-29 11:28:33 +0100

    links

--- src/components/Project-Card/index.js
+++ src/components/Project-Card/index.js
@@ -1,17 +1,16 @@
 import { Card, Button } from "react-bootstrap";
 import React from "react";
 import css from "../../styles/projectcard.module.css"
-import { Link } from "react-router-dom"
 
 
-function ProjectCard({image, title, leftButton, rightButton, leftClick}){
+function ProjectCard({image, title, leftButton, rightButton, leftClick, rightClick}){
     return <div>
     <Card className={css.card}>
     <Card.Img variant="top" src={image} />
     <Card.Body>
     <Card.Title>{title}</Card.Title>
     <Button className={css.button} variant="primary"><a href={leftClick}>{leftButton}</a></Button>
-    <Button className={css.button} variant="primary">{rightButton}</Button>
+    <Button className={css.button} variant="primary"><a href={rightClick}>{rightButton}</a></Button>
     </Card.Body>
     </Card>
 </div>
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -18,13 +18,13 @@ function Projects() {
         </Row>
         <Row className={css.row}>
         <Col className={css.col}>
-        <ProjectCard leftClick={"https://echelonfit.com/blogs/blog/walking-a-path-to-a-healthier-you"} leftButton={'Echelon'} rightButton={'Nashville shoe warehouse'} title={'Blog Work'} image={"https://images.unsplash.com/photo-1500989145603-8e7ef71d639e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80"}/>
+        <ProjectCard rightClick={"https://www.nashvilleshoewarehouse.com/blogs/fashion-trends-news/take-your-style-to-new-heights"} leftClick={"https://echelonfit.com/blogs/blog/walking-a-path-to-a-healthier-you"} leftButton={'Echelon'} rightButton={'Nashville shoe warehouse'} title={'Blog Work'} image={"https://images.unsplash.com/photo-1500989145603-8e7ef71d639e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80"}/>
         </Col>
         <Col className={css.col}>
-        <ProjectCard leftButton={'Echelon'} rightButton={'Nashville shoe warehouse'} title={'E-Commerce'} image={"https://images.unsplash.com/photo-1556740714-a8395b3bf30f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}/>
+        <ProjectCard rightClick={"https://www.nashvilleshoewarehouse.com/collections/slippers/products/levis-harbin-mens-clog-slipper"} leftClick={"https://echelonfit.com/products/echelon-smart-connect-bike-ex5"} leftButton={'Echelon'} rightButton={'Nashville shoe warehouse'} title={'E-Commerce'} image={"https://images.unsplash.com/photo-1556740714-a8395b3bf30f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}/>
         </Col>
         <Col className={css.col}>
-        <ProjectCard leftButton={'Opry'} rightButton={'Roka'} title={'Ideation & Brand Development'} image={"https://images.unsplash.com/photo-1609372697655-65d874e3fe60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}/>
+        <ProjectCard rightClick={"https://www.nashvilleshoewarehouse.com/collections/slippers/products/levis-harbin-mens-clog-slipper"} leftClick={"https://www.opry.com/opry-5000-landing-page/"} leftButton={'Opry'} rightButton={'Roka'} title={'Ideation & Brand Development'} image={"https://images.unsplash.com/photo-1609372697655-65d874e3fe60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}/>
         </Col>
         <Col className={css.col}>
         <ProjectCard leftButton={'Hunter'} rightButton={'Trevecca'} title={'Web Copy'} image={"https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"}/>
