499e1106af76df323aa7e93e54b8a52c68d6ea33
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-05-05 21:29:49 +0100

    change of header

--- src/components/Profile-image/index.js
+++ src/components/Profile-image/index.js
@@ -1,20 +1,13 @@
 import React from "react";
-import { Image, Row, Col } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 import jessy from "../../images/P6130006.jpeg"
 import css from "../../styles/image.module.css"
 
 function ProfileImage(){
-    return <Row className={css.div}>
-    <Col className={css.image}> 
-    <Image src={jessy} roundedCircle/>
-    </Col>
-    <Col className={css.header} >
-        <h1 className={css.text}>Jessy Anne Scott</h1>
-        </Col>
-        <Col className={css.second} >
-        <h1 className={css.text}> Copywriter & Marketing Specialist</h1>
-    </Col>
-    </Row>
+    return <div className={css.div}>
+        <Image className={css.image} src={jessy} roundedCircle/>
+        <h1 className={css.header}>Jessy Anne Scott | Copywriter & Marketing Specialist</h1>
+    </div>
 }
 
 export default ProfileImage
